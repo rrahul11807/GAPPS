344b23cfa24eee00da33f3e42be0e6d87722fd0c
Rahul <rrahul.rajappan@gmail.com>
2019-06-07 22:16:31 +1200

    Version 1

--- app/src/main/java/com/example/gapps/MapsActivity.java
+++ app/src/main/java/com/example/gapps/MapsActivity.java
@@ -2,13 +2,18 @@ package com.example.gapps;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,9 +25,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Location currentLocation;
+    private static final int REQUEST_LOCATION = 99;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -33,13 +41,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         LocationRequest req = new LocationRequest();
-        req.setInterval(2000); // 2 seconds
+        req.setInterval(10000); // 6000 seconds
         req.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
         // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+                != PackageManager.PERMISSION_GRANTED)
+        {
 
             // Permission is not granted
             // Should we show an explanation?
@@ -48,26 +57,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
             }
+        } else {
+            // Permission has already been granted
+
+            client.requestLocationUpdates(req, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    Log.e("location:", locationResult.getLastLocation().toString());
+
+                    Toast.makeText(MapsActivity.this, locationResult.getLastLocation().toString(), Toast.LENGTH_LONG).show();
+                    // Add a marker in Sydney and move the camera
+                    LatLng clocation = new LatLng(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude());
+                    mMap.clear();
+                    mMap.addMarker(new MarkerOptions().position(clocation).title("Current Location"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(clocation,18f));
+
+
+                }
+            }, null);
+
+
+        }
     }
 
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera. In this case,
+         * we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to install
+         * it inside the SupportMapFragment. This method will only be triggered once the user has
+         * installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady (GoogleMap googleMap)
+        {
+            mMap = googleMap;
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+            // Add a marker in Sydney and move the camera
+     //      LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+       //    MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Unitec,Mount Albert");
+         //  googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+           //googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,5));
+           //googleMap.addMarker(markerOptions);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+            // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+
 }
